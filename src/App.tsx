48ef5434e3ee228c1a1c29e0d28f9e9f446b24c4
wigor <wigor21@hotmail.com>
2024-11-17 01:43:29 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Timer</h1>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router/>
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
