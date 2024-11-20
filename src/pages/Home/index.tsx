5aa20c827edec6b46bc8e95c8cf8d79ce0184ff9
wigor <wigor21@hotmail.com>
2024-11-20 00:21:16 -0300

    fix: reset the time in a new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,13 +48,18 @@ export const Home = () => {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
@@ -69,6 +74,7 @@ export const Home = () => {
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
 
     reset();
   };
